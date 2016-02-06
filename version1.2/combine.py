97065b50f05f0918e2f5dc7e7241971bdbade5cb
chicagocomputerclasses <info@chicagocomputerclasses.com>
2016-02-06 00:56:23 -0600

    1.2 dir

--- version1.2/combine.py
+++ version1.2/combine.py
@@ -1,19 +1,24 @@
 import pandas as pd
+import os
 
-ifile = 'sexyonesize'
-ofile = 'combined' + ifile
+ifile = 'lingerie'
+ofile = 'combined-' + ifile
+sfile = 23
 qfile = 49
 
+infolder = 'Excel'
+folder = 'Combined Excel'
+
 def eread(x):
-    df = pd.read_excel(x)
+    df = pd.read_excel(os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + '/' +  infolder + '/' + x)
     return df
 
 a = []
 
-mdf = eread(ifile + '1.xlsx')
+mdf = eread(ifile + str(sfile) + '.xlsx')
 
-for i in range(1,qfile-1):
+for i in range(sfile+1,qfile):
     nd = eread(ifile + str(i+1) + '.xlsx')
     mdf = mdf.merge(nd, how='outer')
 
-mdf.to_excel(ofile + '.xlsx', index=False)
+mdf.to_excel(os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + '/' +  folder + '/' + ofile + '.xlsx', index=False)
